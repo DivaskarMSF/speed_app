70dea11d99ef002cb40af124c567ee52131b5c08
Divasker <divasker.b@marketsimplified.com>
2023-05-31 19:02:17 +0530

    folder changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:speed_app/bloc/speed_bloc.dart';
 import 'dart:math' as math;
 
-import 'package:speed_app/home_screen.dart';
+import 'package:speed_app/screens/home_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
